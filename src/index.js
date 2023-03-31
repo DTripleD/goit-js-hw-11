ded9c992fa13e0ec113ed96fa07d414ea5c4fc32
DTripleD <tripledpadik@gmail.com>
2023-03-31 15:52:41 +0300

    make css styles

--- src/index.js
+++ src/index.js
@@ -1,74 +1,85 @@
-const formEl = document.querySelector(".search-form");
-const gallery = document.querySelector(".gallery");
+const formEl = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
 
-formEl.addEventListener("submit", onSearch);
+formEl.addEventListener('submit', onSearch);
+// loadMore.addEventListener("click", onLoadMoreClick);
 
-function getData(query){
-    const API_KEY = '34891295-3c871ab0268d353f15c88782f';
-    const params = new URLSearchParams({
-        key: API_KEY,
-        q: query,
-        per_page: 40,
-      });
-
-
-    return fetch(`https://pixabay.com/api/?${params}&image_type=photo&orientation=horizontal&safesearch=true`).then(response => response.json())
-    // when markap will be done use lower return !!!!!!!!!!!
-    // return fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${input}&image_type=photo&orientation=horizontal&safesearch=true`).then(response => response.json())
+function onLoadMoreClick() {
+  if (gallery.innerHTML !== '') {
+    return loadMore.classList.remove('visually-hidden');
+  }
 }
 
+function getData(query) {
+  const API_KEY = '34891295-3c871ab0268d353f15c88782f';
+  let page = 1;
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    per_page: 40,
+    page,
+  });
 
+  return fetch(
+    `https://pixabay.com/api/?${params}&image_type=photo&orientation=horizontal&safesearch=true`
+  ).then(response => response.json());
+  // when markap will be done use lower return !!!!!!!!!!!
+  // return fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${input}&image_type=photo&orientation=horizontal&safesearch=true`).then(response => response.json())
+}
 
-
-function markup(data){
-    return data.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => 
-    `<div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+function markup(data) {
+  return data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<div class="photo-card">
+        <img class="gallery__item" src="${webformatURL}" alt="${tags}" loading="lazy" />
         <div class="info">
             <p class="info-item">
-                <b>${likes}</b>
+                <b>Likes: </b>
+                ${likes}
             </p>
             <p class="info-item">
-                <b>${views}</b>
+                <b>Views: </b>
+                ${views}
             </p>
             <p class="info-item">
-                <b>${comments}</b>
+                <b>Comments: </b>
+                ${comments}
             </p>
             <p class="info-item">
-                <b>${downloads}</b>
+                <b>Downloads: </b>
+                ${downloads}
             </p>
         </div>
-    </div>`)
+    </div>`
+    )
+    .join('');
 }
 
-function onSearch(event){
-    event.preventDefault();
+function onSearch(event) {
+  event.preventDefault();
 
-    const inputValue = event.currentTarget.searchQuery.value;
+  onLoadMoreClick();
 
+  const inputValue = event.currentTarget.searchQuery.value;
 
-    getData(inputValue).then((data) => {
-        if(data.hits === {}){
-            return console.log("Sorry, there are no images matching your search query. Please try again.");
-        }
+  getData(inputValue).then(data => {
+    // if (data.hits === {}) {
+    //   return console.log(
+    //     'Sorry, there are no images matching your search query. Please try again.'
+    //   );
+    // }
+    return console.log(data);
 
-        gallery.innerHTML = markup(data.hits);
-    
-    });
-
-
-
-    
-
-
-
-
-    
-
-    
+    gallery.innerHTML = markup(data.hits);
+  });
 }
-
-
-
-
-
