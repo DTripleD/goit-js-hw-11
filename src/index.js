a983166761df334f5f678b65b52e21cb26fa4ea3
DTripleD <tripledpadik@gmail.com>
2023-04-01 15:15:40 +0300

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import './css/styles.css';
 import GetData from './js/fetch';
-import {lightbox} from './js/lightbox'
-
+import { lightbox } from './js/lightbox';
+import Notiflix from 'notiflix';
 
 const getData = new GetData();
-
 
 const formEl = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
@@ -12,10 +12,61 @@ const loadMore = document.querySelector('.load-more');
 formEl.addEventListener('submit', onSearch);
 loadMore.addEventListener('click', onMakingMarkup);
 
+function onSearch(event) {
+  event.preventDefault();
 
+  gallery.innerHTML = '';
+
+  const input = event.currentTarget.searchQuery.value.trim();
+  getData.resetPage();
+
+  getData.inputValue = input;
+
+  if (getData.searchQuery.trim() === '') {
+    Notiflix.Notify.warning('Please, fill the main field');
+    return;
+  }
+
+  onMakingMarkup();
+
+}
+
+
+async function onMakingMarkup() {
+  loadMore.classList.add('visually-hidden');
+  const response = await getData.onFetch();
+  const { hits, totalHits } = response;
+
+  if (hits.length === 0) {
+    gallery.innerHTML = '';
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+
+  if (gallery.children.length === 0) {
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+  }
+
+  markup(hits);
+
+  if (gallery.children.length < totalHits) {
+    loadMore.classList.remove('visually-hidden');
+  }
+
+  if (gallery.children.length >= totalHits) {
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+    return loadMore.classList.add('visually-hidden');
+  }
+
+
+}
 
 function markup(data) {
-  return data
+  const markup = data
     .map(
       ({
         webformatURL,
@@ -25,70 +76,34 @@ function markup(data) {
         views,
         comments,
         downloads,
-      }) => 
-        {
-            return `<div class="photo-card">
-            <a href="${largeImageURL}">
-              <img class="gallery__item" src="${webformatURL}" alt="${tags}" loading="lazy">
-            </a>
-            <div class="info">
-                <p class="info-item">
-                    <b>Likes: </b>
-                    ${likes}
-                </p>
-                <p class="info-item">
-                    <b>Views: </b>
-                    ${views}
-                </p>
-                <p class="info-item">
-                    <b>Comments: </b>
-                    ${comments}
-                </p>
-                <p class="info-item">
-                    <b>Downloads: </b>
-                    ${downloads}
-                </p>
-            </div>
-        </div>`
-        }
-
+      }) => {
+        return `<div class="photo-card">
+              <a href="${largeImageURL}">
+                <img class="gallery__item" src="${webformatURL}" alt="${tags}" loading="lazy">
+              </a>
+              <div class="info">
+                  <p class="info-item">
+                      <b>Likes: </b>
+                      ${likes}
+                  </p>
+                  <p class="info-item">
+                      <b>Views: </b>
+                      ${views}
+                  </p>
+                  <p class="info-item">
+                      <b>Comments: </b>
+                      ${comments}
+                  </p>
+                  <p class="info-item">
+                      <b>Downloads: </b>
+                      ${downloads}
+                  </p>
+              </div>
+          </div>`;
+      }
     )
     .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
 }
-
-function onSearch(event) {
-  event.preventDefault();
-
-  gallery.innerHTML = '';
-  getData.resetPage();
-
-  const input = event.currentTarget.searchQuery.value.trim();
-
-  getData.inputValue = input;
-
-  onMakingMarkup();
-}
-
-async function onMakingMarkup() {
-  const response = await getData.onFetch();
-  const { hits, totalHits } = response;
-
-  console.log(`Hooray! We found ${totalHits}npm install simplelightbox images.`);
-
-    if (hits.length === 0) {
-      loadMore.classList.add('visually-hidden');
-      gallery.innerHTML = '';
-      return console.log('Sorry, there are no images matching your search query. Please try again.');
-    }
-    lightbox.refresh();
-    gallery.insertAdjacentHTML('beforeend', markup(hits));
-
-    if (gallery.children.length >= totalHits) {
-      console.log("We're sorry, but you've reached the end of search results.");
-      return loadMore.classList.add('visually-hidden');
-    }
-
-    loadMore.classList.remove('visually-hidden');
-  };
-
-
